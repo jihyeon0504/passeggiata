32205ef181e9f53f9e9e30942776856f185bf94d
hong jihyeon <hjh@eplatform.co.kr>
2023-09-26 17:20:21 +0900

    프랜차이즈 이미지 슬라이드 및 하이브리드 내용추가

--- html/js/ui.js
+++ html/js/ui.js
@@ -1,1 +1,10 @@
-//  스크롤이벤트
+//  스크롤 헤더
+$(function(){
+   $(document).on('scroll',function(){
+    if($(window).scrollTop() > 30){
+        $('header').addClass('act');
+    }else{
+        $('header').removeClass('act');
+    }
+   })
+})
